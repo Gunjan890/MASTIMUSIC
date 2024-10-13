9c0ef1489ef8fa968677cd7c160881babba6a716
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-13 15:24:46 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -71,7 +71,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_apna_gunjan_II"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+TkkVwWjSqWc5MDc1"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+Jlu3yXB5yc4wODRl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -93,7 +93,7 @@ def stream_markup(_, videoid, chat_id):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_apna_gunjan_II"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+TkkVwWjSqWc5MDc1"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+Jlu3yXB5yc4wODRl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -248,7 +248,7 @@ def stream_markup2(_, chat_id):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_apna_gunjan_II"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+TkkVwWjSqWc5MDc1"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+Jlu3yXB5yc4wODRl"
             ),
         ],
 
